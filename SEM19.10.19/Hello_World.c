43b07c9bff59f23347a96acb8c842d8011bbf1a6
KORCH <korch@Andrey.localdomain>
2019-10-22 16:51:35 +0300

    21.10.2019

--- SEM19.10.19/Hello_World.c
+++ SEM19.10.19/Hello_World.c
@@ -1,16 +1,22 @@
-/* 
-* hello−1.c − The simplest kernel module. 
+/*
+* hello-1.c - Простейший модуль ядра.
 */
-#include <linux/module.h> /* Needed by all modules */ 
-#include <linux/kernel.h> /* Needed for KERN_INFO */
-int init_module(void){ 
-	printf(KERN_INFO "Hello world 1.\n");
-	/* 
-	* A non 0 return means init_module failed; module can't be loaded.
+#include <linux/module.h> /* Необходим для любого модуля ядра */ 
+#include <linux/kernel.h> /* Здесь находится 
+#определение KERN_ALERT */
+
+void cleanuo_module(void);
+
+int init_module(init_module){
+	printk("<1>Hello world 1.\n");
+	/*
+	* Если вернуть ненулевое значение, то это будет 
+   	воспринято как признак ошибки, * возникшей в процессе работы init_module; в результате модуль не будет 
+   	загружен.
 	*/
-	return 0;
+return 0;
 }
 
-void cleanup_module(void){
-	printf(KERN_INFO "Goodbye world 1.\n");
+void cleanup_module(cleanup_module){
+	printk(KERN_ALERT "Goodbye world 1.\n");
 }
