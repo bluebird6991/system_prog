0825091a9a9d97b7f5341e80da92ea56e47dc5ef
AndreyKorchagin <korchagin.andrey.i@ya.ru>
2020-03-05 21:34:30 +0300

    Socet

--- Socets/client.c
+++ Socets/client.c
@@ -1,68 +1,76 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
- 
-int main (int argc, char **argv)
-{
-char message[2048];//=(char*)malloc(sizeof(char));// сообщение на передачу
-char buf[sizeof(message)];
-int port,ch;
-//----------------------------------------------------------------------------
-if(argc!=3){
-printf("Не верное кол-во аргументов!\nДолжно быть 2 аргумента (Порт,ip-адрес -сервера)!\n");
-exit(0);
-}
- 
-    int sock;                 // дескриптор сокета
-    struct sockaddr_in addr; // структура с адресом
-    struct hostent* hostinfo;
-port = atoi(argv[1]);
-hostinfo = argv[2];
-    sock = socket(AF_INET, SOCK_STREAM, 0); // создание TCP-сокета
-    if(sock < 0)
-    {
-        perror("socket");
-        exit(1);
-    }
- 
-    // Указываем параметры сервера
-    addr.sin_family = AF_INET; // домены Internet
-    addr.sin_port = htons(port); // или любой другой порт...
-    addr.sin_addr.s_addr=inet_addr(hostinfo);
-    /*
-     * Начинающие программисты часто спрашивают, как можно отлаживать сетевую программу, если под рукой нет сети.
-     * Оказывается, можно обойтись и без неё. Достаточно запустить клиента и сервера на одной машине, а затем использовать для соединения адрес интерфейса внутренней петли (loopback interface).
-     * В программе ему соответствует константа INADDR_LOOPBACK (не забудьте применять к ней функцию htonl!).
-     * Пакеты, направляемые по этому адресу, в сеть не попадают.
-     * Вместо этого они передаются стеку протоколов TCP/IP как только что принятые.
-     * Таким образом моделируется наличие виртуальной сети, в которой вы можете отлаживать ваши сетевые приложения.
-     */
-   // addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
-      //addr.sin_addr.s_addr = inet_addr(host_ip);
- if(connect(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0) // установка соединения с сервером
-    {
-        perror("Подключение");
-        exit(2);
-    }
- 
-while(1){//WHILE <---
-//----------------------------------------------------------------------------
-    printf("Введите сообщение серверу(Для выхода:exit): "); 
-    if (!strcmp(gets(message), "exit")){close(sock);return 0;}
-//----------------------------------------------------------------------------
- 
-    printf("отправка сообщения на сервер...\n");
-    send(sock, message, sizeof(message), 0); // отправка сообщения на сервер
-    int bytes_read = 0;
-    printf("Ожидание сообщения\n");
-    bytes_read = recv(sock, buf, sizeof(message), 0);
-    printf("Получено %d bytes\tСообщение: %s\n", bytes_read, buf); // прием сообщения от сервера
+
+#include <netdb.h>
+#include <netinet/in.h>
+
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+   int sockfd, portno, n;
+   struct sockaddr_in serv_addr;
+   struct hostent *server;
+   
+   char buffer[256];
+   
+   if (argc < 3) {
+      fprintf(stderr,"usage %s hostname port\n", argv[0]);
+      exit(0);
+   }
     
- 
-}//END_WHILE
+   portno = atoi(argv[2]);
+   
+   /* Create a socket point */
+   sockfd = socket(AF_INET, SOCK_STREAM, 0);
+   
+   if (sockfd < 0) {
+      perror("ERROR opening socket");
+      exit(1);
+   }
     
-return 0;
+   server = gethostbyname(argv[1]);
+   
+   if (server == NULL) {
+      fprintf(stderr,"ERROR, no such host\n");
+      exit(0);
+   }
+   
+   bzero((char *) &serv_addr, sizeof(serv_addr));
+   serv_addr.sin_family = AF_INET;
+   bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+   serv_addr.sin_port = htons(portno);
+   
+   /* Now connect to the server */
+   if (connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+      perror("ERROR connecting");
+      exit(1);
+   }
+   
+   /* Now ask for a message from the user, this message
+      * will be read by server
+   */
     
+   printf("Please enter the message: ");
+   bzero(buffer,256);
+   fgets(buffer,255,stdin);
+   
+   /* Send message to the server */
+   n = write(sockfd, buffer, strlen(buffer));
+   
+   if (n < 0) {
+      perror("ERROR writing to socket");
+      exit(1);
+   }
+   
+   /* Now read server response */
+   bzero(buffer,256);
+   n = read(sockfd, buffer, 255);
+   
+   if (n < 0) {
+      perror("ERROR reading from socket");
+      exit(1);
+   }
+    
+   printf("%s\n",buffer);
+   return 0;
 }
--- Socets/server.c
+++ Socets/server.c
@@ -1,74 +1,72 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+#include <netdb.h>
+#include <netinet/in.h>
+
 #include <string.h>
- 
-int main()
-{
-    int sock, listener;       // дескрипторы сокетов
-    struct sockaddr_in addr; // структура с адресом
-    char buf[2048];       // буфур для приема
-    int bytes_read;           // кол-во принятых байт
- 
- 
-    listener = socket(AF_INET, SOCK_STREAM, 0); // создаем сокет для входных подключений
-    if(listener < 0)
-    {
-        perror("socket");
-        exit(1);
-    }
+
+int main( int argc, char *argv[] ) {
+   int sockfd, newsockfd, portno, clilen;
+   char buffer[256];
+   struct sockaddr_in serv_addr, cli_addr;
+   int  n;
+   
+   /* First call to socket() function */
+   sockfd = socket(AF_INET, SOCK_STREAM, 0);
+   
+   if (sockfd < 0) {
+      perror("ERROR opening socket");
+      exit(1);
+   }
+   
+   /* Initialize socket structure */
+   bzero((char *) &serv_addr, sizeof(serv_addr));
+   portno = 5001;
+   
+   serv_addr.sin_family = AF_INET;
+   serv_addr.sin_addr.s_addr = INADDR_ANY;
+   serv_addr.sin_port = htons(portno);
+   
+   /* Now bind the host address using bind() call.*/
+   if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+      perror("ERROR on binding");
+      exit(1);
+   }
+      
+   /* Now start listening for the clients, here process will
+      * go in sleep mode and will wait for the incoming connection
+   */
+   
+   listen(sockfd,5);
+   clilen = sizeof(cli_addr);
+   
+   /* Accept actual connection from the client */
+   newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
     
-    // Указываем параметры сервера
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(3425);
-    //addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    addr.sin_addr.s_addr=inet_addr("127.0.0.1");
-    if(bind(listener, (struct sockaddr *)&addr, sizeof(addr)) < 0) // связываемся с сетевым устройство. Сейчас это устройство lo - "петля", которое используется для отладки сетевых приложений
-    {
-        perror("bind");
-        exit(2);
-    }
- 
-    listen(listener, 1); // очередь входных подключений
-    
-    while(1)
-    {
-        sock = accept(listener, NULL, NULL); // принимаем входные подключение и создаем отделный сокет для каждого нового подключившегося клиента
-        if(sock < 0)
-        {
-            perror("Прием входящих подключений");
-            exit(3);
-        }
- 
-switch(fork())
-{
-case -1:
- perror("fork");
- break;
-case 0:
-close(listener);
- 
- 
-        while(1)
-        {
-            printf("Ожидаем сообщение...\n");
- 
- 
-            bytes_read = recv(sock, buf, 2048, 0); // принимаем сообщение от клиента
-            if(bytes_read <= 0) break;
-            printf("Получено %d bytes\tСообщение: %s\n", bytes_read, buf);
-            printf("Отправляю принятое сообщение клиенту\n");
-            send(sock, buf, bytes_read, 0); // отправляем принятое сообщение клиенту
-        }
-    
-        close(sock); // закрываем сокет
-        _exit(0);
-default:
-close(sock);
-}   
-    }
-    
-    return 0;
+   if (newsockfd < 0) {
+      perror("ERROR on accept");
+      exit(1);
+   }
+   
+   /* If connection is established then start communicating */
+   bzero(buffer,256);
+   n = read( newsockfd,buffer,255 );
+   
+   if (n < 0) {
+      perror("ERROR reading from socket");
+      exit(1);
+   }
+   
+   printf("Here is the message: %s\n",buffer);
+   
+   /* Write a response to the client */
+   n = write(newsockfd,"I got your message",18);
+   
+   if (n < 0) {
+      perror("ERROR writing to socket");
+      exit(1);
+   }
+      
+   return 0;
 }
