9d11fbe9268884bfc0577c52096ea3f0b0acf2af
AndreyKorchagin <korchagin.andrey.i@ya.ru>
2020-03-07 00:26:28 +0300

    Chardevice

--- CharDevice/CharDevice.c
+++ CharDevice/CharDevice.c
@@ -29,38 +29,34 @@ static char msg[BUF_LEN];     /* Здесь будет собираться те
 static char *msg_Ptr;
 
 static struct file_operations fops = {
-  .read = device_read,
-  .write = device_write,
-  .open = device_open,
-  .release = device_release
+    .read = device_read,
+    .write = device_write,
+    .open = device_open,
+    .release = device_release
 };
 
 /*
  * Функции
  */
 
-int init_module(void)
-{
-  Major = register_chrdev(0, DEVICE_NAME, &fops);
+int init_module(void){
+    Major = register_chrdev(0, DEVICE_NAME, &fops);
 
-  if (Major < 0) {    
-    printk("Registering the character device failed with %d\n",
-            Major);
-    return Major;
-  }
+    if (Major < 0) {       
+        printk("Registering the character device failed with %d\n", Major);
+        return Major;
+    }
+    printk("<1>I was assigned major number %d.  To talk to\n", Major);
+    printk("<1>the driver, create a dev file with\n");
+    printk("'mknod /dev/chardev c %d 0'.\n", Major);
+    printk("<1>Try various minor numbers.  Try to cat and echo to\n");
+    printk("the device file.\n");
+    printk("<1>Remove the device file and module when done.\n");
 
-  printk("<1>I was assigned major number %d.  To talk to\n", Major);
-  printk("<1>the driver, create a dev file with\n");
-  printk("'mknod /dev/chardev c %d 0'.\n", Major);
-  printk("<1>Try various minor numbers.  Try to cat and echo to\n");
-  printk("the device file.\n");
-  printk("<1>Remove the device file and module when done.\n");
-
-  return 0;
+    return 0;
 }
 
-void cleanup_module(void)
-{
+void cleanup_module(void){
   /* 
    * Отключение устройства 
    */
@@ -77,59 +73,57 @@ void cleanup_module(void)
  * Вызывается, когда процесс пытается открыть файл устройства, например командой
  * "cat /dev/chardev"
  */
-static int device_open(struct inode *inode, struct file *file)
-{
-  static int counter = 0;
-  if (Device_Open)
-    return -EBUSY;
-  Device_Open++;
-  sprintf(msg, "I already told you %d times Hello world!\n", counter++);
-  msg_Ptr = msg;
-  try_module_get(THIS_MODULE);
+static int device_open(struct inode *inode, struct file *file){
+    static int counter = 0;
+    if (Device_Open)
+        return -EBUSY;
+    Device_Open++;
+    sprintf(msg, "I already told you %d times Hello world!\n", counter++);
+    msg_Ptr = msg;
+    try_module_get(THIS_MODULE);
 
-  return SUCCESS;
+    return SUCCESS;
 }
 
 /* 
  * Вызывается, когда процесс закрывает файл устройства.
  */
-static int device_release(struct inode *inode, struct file *file)
-{
-  Device_Open--;                /* Теперь мы готовы обслужить другой процесс */
+static int device_release(struct inode *inode, struct file *file){
+    Device_Open--;                /* Теперь мы готовы обслужить другой процесс */
 
   /* 
    * Уменьшить счетчик обращений, иначе, после первой же удачной попытки открыть файл устройства,
    * вы никогда не сможете выгрузить модуль. 
    */
-  module_put(THIS_MODULE);
+    module_put(THIS_MODULE);
 
-  return 0;
+    return 0;
 }
 
 /* 
  * Вызывается, когда процесс пытается прочитать уже открытый файл устройства
  */
-static ssize_t device_read(struct file *filp, /* см. include/linux/fs.h   */
-         char *buffer,                        /* буфер, куда надо положить данные */
-         size_t length,                       /* размер буфера */
-         loff_t * offset)
-{
+static ssize_t device_read(     struct file *filp, /* см. include/linux/fs.h   */
+                                char *buffer,      /* буфер, куда надо положить данные */
+                                size_t length,     /* размер буфера */
+                                loff_t *offset){
   /*
    * Количество байт, фактически записанных в буфер
    */
-  int bytes_read = 0;
+    int bytes_read = 0;
 
   /*
    * Если достигли конца сообщения, 
    * вернуть 0, как признак конца файла
    */
-  if (*msg_Ptr == 0)
-    return 0;
+    if (*msg_Ptr == 0){
+        return 0;
+    }
 
   /* 
    * Перемещение данных в буфер
    */
-  while (length && *msg_Ptr) {
+    while (length && *msg_Ptr){
 
     /* 
      * Буфер находится в пространстве пользователя (в сегменте данных), 
@@ -137,25 +131,54 @@ static ssize_t device_read(struct file *filp, /* см. include/linux/fs.h   */
      * Для того, чтобы скопировать данные, мы используем функцию put_user, 
      * которая перенесет данные из пространства ядра в пространство пользователя. 
      */
-    put_user(*(msg_Ptr++), buffer++);
+        put_user(*(msg_Ptr++), buffer++);
 
-    length--;
-    bytes_read++;
-        }
+        length--;
+        bytes_read++;
+    }
 
   /* 
    * В большинстве своем, функции чтения возвращают количество байт, записанных в буфер.
    */
-  return bytes_read;
+    return bytes_read;
 }
 
 /*  
  * Вызывается, когда процесс пытается записать в устройство, 
  * например так: echo "hi" > /dev/chardev
  */
-static ssize_t
-device_write(struct file *filp, const char *buff, size_t len, loff_t * off)
-{
-  printk("<1>Sorry, this operation isn't supported.\n");
-  return -EINVAL;
+static ssize_t device_write(    struct file *filp, 
+                                const char *buff,
+                                size_t len,
+                                loff_t * off){
+
+    /*int bytes_write = 0;
+    if(*msg_Ptr == 0){
+        return 0;
+    }
+
+    while(length && *msg_Ptr){
+        get_user(*(msg_Ptr++), buffer++);
+        length--;
+        bytes_read++;
+    }
+
+    printk("<1>Sorry, this operation isn't supported.\n");
+*/
+    int i;
+
+#ifdef DEBUG
+    printk(KERN_INFO "device_write(%p,%s,%d)", file, buffer, length);
+#endif
+
+    for (i = 0; i < length && i < BUF_LEN; i++)
+        get_user(Message[i], buffer + i);
+
+    Message_Ptr = Message;
+
+    /* 
+     * Again, return the number of input characters used 
+     */
+    return i;
+    //return -EINVAL;
 }
