64136a355be1e7dd315098a556227d047a812413
AndreyKorchagin <korchagin.andrey.i@ya.ru>
2020-03-07 13:04:47 +0300

    Procfs

---
+++ procfs/proc.c
@@ -0,0 +1,88 @@
+#include <linux/module.h> 
+#include <linux/kernel.h> 
+#include <linux/proc_fs.h>
+/* Specifically, a module */
+/* We're doing kernel work */
+/* Necessary because we use the proc fs */
+#define procfs_name "helloworld"
+/**
+ * This structure hold information about the /proc file
+ *
+ */
+struct proc_dir_entry *Our_Proc_File;
+/* Put data into the proc fs file. *
+* Arguments
+* =========
+* 1. *
+* 2. *
+*
+* 3.
+* 4.
+* 5.
+* 6.
+*
+*
+* Usage and Return Value
+* ======================
+* A return value of zero means you have no further
+* information at this time (end of file). A negative
+* return value is an error condition. *
+* For More Information
+* ====================
+* The way I discovered what to do with this function * wasn't by reading documentation, but by reading the
+* code which used it. I just looked to see what uses
+* the get_info field of proc_dir_entry struct (I used a * combination of find and grep, if you're interested), * and I saw that it is used in <kernel source
+* directory>/fs/proc/array.c.
+*
+* If something is unknown about the kernel, this is * usually the way to go. In Linux we have the great * advantage of having the kernel source code for
+* free - use it.
+*/
+int procfile_read(  char *buffer,
+                    char **buffer_location,
+                    off_t offset,
+                    int buffer_length,
+                    int *eof,
+                    void *data){
+    int ret;
+    printk(KERN_INFO "procfile_read (/proc/%s) called\n", procfs_name);
+/*
+ * We give all of our information in one go, so if the
+ * user asks us if we have more information the
+ * answer should always be no.
+*
+* This is important because the standard read
+* function from the library would continue to issue * the read system call until the kernel replies
+* that it has no more information, or until its
+* buffer is filled.
+*/
+    if (offset > 0) {
+        /* we have finished to read, return 0 */ 
+        ret = 0;
+    } else {
+        /* fill the buffer, return the buffer size */
+        ret = sprintf(buffer, "HelloWorld!\n");
+    }
+    return ret;
+}
+
+int init_module() {
+    Our_Proc_File = create_proc_entry(procfs_name, 0644, NULL);
+    if (Our_Proc_File == NULL) {
+        remove_proc_entry(procfs_name, &proc_root);
+        printk(KERN_ALERT "Error: Could not initialize /proc/%s\n", procfs_name); 
+        return -ENOMEM;
+    }
+    Our_Proc_File->read_proc    = procfile_read;
+    Our_Proc_File->owner        = THIS_MODULE;
+    Our_Proc_File->mode         = S_IFREG | S_IRUGO;
+    Our_Proc_File->uid          = 0;
+    Our_Proc_File->gid          = 0;
+    Our_Proc_File->size         = 37;
+
+    printk(KERN_INFO "/proc/%s created\n", procfs_name);
+    return 0; /* everything is ok */
+
+void cleanup_module(){
+    remove_proc_entry(procfs_name, &proc_root);
+    printk(KERN_INFO "/proc/%s removed\n", procfs_name);
+}
--- procfs/procfs.c
+++ procfs/procfs.c
@@ -1,88 +1,61 @@
-#include <linux/module.h> 
-#include <linux/kernel.h> 
-#include <linux/proc_fs.h>
-/* Specifically, a module */
-/* We're doing kernel work */
-/* Necessary because we use the proc fs */
-#define procfs_name "helloworld"
-/**
- * This structure hold information about the /proc file
- *
- */
-struct proc_dir_entry *Our_Proc_File;
-/* Put data into the proc fs file. *
-* Arguments
-* =========
-* 1. *
-* 2. *
-*
-* 3.
-* 4.
-* 5.
-* 6.
-*
-*
-* Usage and Return Value
-* ======================
-* A return value of zero means you have no further
-* information at this time (end of file). A negative
-* return value is an error condition. *
-* For More Information
-* ====================
-* The way I discovered what to do with this function * wasn't by reading documentation, but by reading the
-* code which used it. I just looked to see what uses
-* the get_info field of proc_dir_entry struct (I used a * combination of find and grep, if you're interested), * and I saw that it is used in <kernel source
-* directory>/fs/proc/array.c.
-*
-* If something is unknown about the kernel, this is * usually the way to go. In Linux we have the great * advantage of having the kernel source code for
-* free - use it.
-*/
-int procfile_read(  char *buffer,
-                    char **buffer_location,
-                    off_t offset,
-                    int buffer_length,
-                    int *eof,
-                    void *data){
-    int ret;
-    printk(KERN_INFO "procfile_read (/proc/%s) called\n", procfs_name);
-/*
- * We give all of our information in one go, so if the
- * user asks us if we have more information the
- * answer should always be no.
-*
-* This is important because the standard read
-* function from the library would continue to issue * the read system call until the kernel replies
-* that it has no more information, or until its
-* buffer is filled.
-*/
-    if (offset > 0) {
-        /* we have finished to read, return 0 */ 
-        ret = 0;
-    } else {
-        /* fill the buffer, return the buffer size */
-        ret = sprintf(buffer, "HelloWorld!\n");
-    }
-    return ret;
+#include<linux/module.h>
+#include<linux/init.h>
+#include<linux/proc_fs.h>
+#include<linux/sched.h>
+#include<linux/uaccess.h>
+#include<linux/fs.h>
+#include<linux/seq_file.h>
+#include<linux/slab.h>
+
+static char *str = NULL;
+
+static int my_proc_show(struct seq_file *m,void *v){
+	//seq_printf(m,"hello from proc file\n");
+	seq_printf(m,"%s\n",str);
+	return 0;
 }
 
-int init_module() {
-    Our_Proc_File = create_proc_entry(procfs_name, 0644, NULL);
-    if (Our_Proc_File == NULL) {
-        remove_proc_entry(procfs_name, &proc_root);
-        printk(KERN_ALERT "Error: Could not initialize /proc/%s\n", procfs_name); 
-        return -ENOMEM;
-    }
-    Our_Proc_File->read_proc    = procfile_read;
-    Our_Proc_File->owner        = THIS_MODULE;
-    Our_Proc_File->mode         = S_IFREG | S_IRUGO;
-    Our_Proc_File->uid          = 0;
-    Our_Proc_File->gid          = 0;
-    Our_Proc_File->size         = 37;
-
-    printk(KERN_INFO "/proc/%s created\n", procfs_name);
-    return 0; /* everything is ok */
-
-void cleanup_module(){
-    remove_proc_entry(procfs_name, &proc_root);
-    printk(KERN_INFO "/proc/%s removed\n", procfs_name);
+static ssize_t my_proc_write(struct file* file,const char __user *buffer,size_t count,loff_t *f_pos){
+	char *tmp = kzalloc((count+1),GFP_KERNEL);
+	if(!tmp)return -ENOMEM;
+	if(copy_from_user(tmp,buffer,count)){
+		kfree(tmp);
+		return EFAULT;
+	}
+	kfree(str);
+	str=tmp;
+	return count;
 }
+
+static int my_proc_open(struct inode *inode,struct file *file){
+	return single_open(file,my_proc_show,NULL);
+}
+
+static struct file_operations my_fops={
+	.owner = THIS_MODULE,
+	.open = my_proc_open,
+	.release = single_release,
+	.read = seq_read,
+	.llseek = seq_lseek,
+	.write = my_proc_write
+};
+
+static int __init hello_init(void){
+	struct proc_dir_entry *entry;
+	entry = proc_create("helloproc",0777,NULL,&my_fops);
+	if(!entry){
+		return -1;	
+	}else{
+		printk(KERN_INFO "create proc file successfully\n");
+	}
+	return 0;
+}
+
+static void __exit hello_exit(void){
+	remove_proc_entry("helloproc",NULL);
+	printk(KERN_INFO "Goodbye world!\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
+MODULE_LICENSE("GPL");
