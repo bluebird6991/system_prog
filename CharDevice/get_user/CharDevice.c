84130e98037877e59ce538f76f8d73a2203bced5
AndreyKorchagin <korchagin.andrey.i@ya.ru>
2020-03-07 11:43:19 +0300

    New

--- CharDevice/get_user/CharDevice.c
+++ CharDevice/get_user/CharDevice.c
@@ -8,6 +8,8 @@
 MODULE_DESCRIPTION("Device Driver Demo");
 //M0DULE_AUTH0R("Appu Sajeev");
 
+#define DEVICE_NAME "chardev"
+
 static char msg[100]={0};
 static short readPos=0;
 static int times = 0;
@@ -29,9 +31,8 @@ static struct file_operations fops =
 };
 
 // called when module is loaded, similar to main()
-int init_module(void)
-{
-		t = register_chrdev(0,"myDev",&fops); //register driver with major:89
+int init_module(void){
+		t = register_chrdev(0, DEVICE_NAME, &fops); //register driver with major:89
 
 		if (t < 0 ){
 			printk(KERN_ALERT "Device registration failed..\n");
@@ -43,9 +44,8 @@ int init_module(void)
 
 
 // called when module is unloaded, similar to destructor in OOP
-void cleanup_module(void)
-{
-        unregister_chrdev(t, "myDev");
+void cleanup_module(void){
+        unregister_chrdev(t, DEVICE_NAME);
 }
 
 // called when 'open' system call is done on the device file
@@ -59,8 +59,7 @@ static int dev_open(struct inode *inod,struct file *fil){
 // called when 'read' system call is done on the device file
 static ssize_t dev_read(struct file *filp,char *buff,size_t len, loff_t *off){
 		short count = 0;
-		while (len &&(msg[readPos]!=	0))
-		{
+		while (len &&(msg[readPos]!=	0)){
 				put_user(msg[readPos],buff++); //copy byte from kernel space to user space
 				count++;
 				len--;
@@ -70,8 +69,10 @@ static ssize_t dev_read(struct file *filp,char *buff,size_t len, loff_t *off){
 }
 
 // called when 'write' system call is done on the device file
-static ssize_t dev_write(struct file *filp,const char *buff,size_t len,loff_t *off)
-{
+static ssize_t dev_write(	struct file *filp,
+							const char *buff,
+							size_t len,
+							loff_t *off){
 		short ind = len-1;
 		short count=0;
 		memset(msg,0,100);
@@ -84,8 +85,7 @@ static ssize_t dev_write(struct file *filp,const char *buff,size_t len,loff_t *o
 		return count;
 }
 //called when 'close' system call is done on the device files
-static int dev_rls(struct inode *inod,struct file *fil)
-{
+static int dev_rls(struct inode *inod,struct file *fil){
 	printk(KERN_ALERT"Device closed \n");
 	return 0;
 }
