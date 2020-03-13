81068e33f7631eb5bc478de4df0fb77e2f834600
AndreyKorchagin <korchagin.andrey.i@ya.ru>
2020-03-13 23:28:37 +0300

    ioctl

--- ioctl/ioctl.c
+++ ioctl/ioctl.c
@@ -9,7 +9,6 @@
  * Определения старшего номера устройства и коды операций ioctl
  */
 #include "chardev.h"
-
 #include <fcntl.h>      /* open */
 #include <unistd.h>     /* exit */
 #include <sys/ioctl.h>  /* ioctl */
@@ -18,22 +17,20 @@
  * Функции работы с драйвером через ioctl  
  */
 
-ioctl_set_msg(int file_desc, char *message)
-{
-  int ret_val;
+ioctl_set_msg(int file_desc, char *message){
+    int ret_val;
 
-  ret_val = ioctl(file_desc, IOCTL_SET_MSG, message);
+    ret_val = ioctl(file_desc, IOCTL_SET_MSG, message);
 
-  if (ret_val < 0) {
-    printf("Ошибка при вызове ioctl_set_msg: %d\n", ret_val);
-    exit(-1);
-  }
+    if (ret_val < 0) {
+        printf("Ошибка при вызове ioctl_set_msg: %d\n", ret_val);
+        exit(-1);
+      }
 }
 
-ioctl_get_msg(int file_desc)
-{
-  int ret_val;
-  char message[100];
+ioctl_get_msg(int file_desc){
+    int ret_val;
+    char message[100];
 
   /* 
    * Внимание - ядро понятия не имеет -- какой длины буфер мы используем
@@ -42,55 +39,51 @@ ioctl_get_msg(int file_desc)
    * передачу в ioctl двух дополнительных параметров:
    * собственно буфера сообщения и его длину
    */
-  ret_val = ioctl(file_desc, IOCTL_GET_MSG, message);
+    ret_val = ioctl(file_desc, IOCTL_GET_MSG, message);
 
-  if (ret_val < 0) {
-    printf("Ошибка при вызове ioctl_get_msg: %d\n", ret_val);
-    exit(-1);
-  }
-
-  printf("Получено сообщение (get_msg): %s\n", message);
-}
-
-ioctl_get_nth_byte(int file_desc)
-{
-  int i;
-  char c;
-
-  printf("N-ный байт в сообщении (get_nth_byte): ");
-
-  i = 0;
-  while (c != 0) {
-    c = ioctl(file_desc, IOCTL_GET_NTH_BYTE, i++);
-
-    if (c < 0) {
-      printf
-        ("Ошибка при вызове ioctl_get_nth_byte на %d-м байте.\n", i);
-      exit(-1);
+    if (ret_val < 0) {
+        printf("Ошибка при вызове ioctl_get_msg: %d\n", ret_val);
+        exit(-1);
     }
 
-    putchar(c);
-  }
-  putchar('\n');
+    printf("Получено сообщение (get_msg): %s\n", message);
+}
+
+ioctl_get_nth_byte(int file_desc){
+    int i;
+    char c;
+
+    printf("N-ный байт в сообщении (get_nth_byte): ");
+
+    i = 0;
+    while (c != 0) {
+        c = ioctl(file_desc, IOCTL_GET_NTH_BYTE, i++);
+
+        if (c < 0) {
+            printf("Ошибка при вызове ioctl_get_nth_byte на %d-м байте.\n", i);
+            exit(-1);
+        }
+
+        putchar(c);
+    }
+    putchar('\n');
 }
 
 /* 
  * Main - Проверка работоспособности функции ioctl
  */
-main()
-{
-  int file_desc, ret_val;
-  char *msg = "Это сообщение передается через ioctl\n";
+main(){
+    int file_desc, ret_val;
+    char *msg = "Это сообщение передается через ioctl\n";
+    file_desc = open(DEVICE_FILE_NAME, 0);
+    if (file_desc < 0) {
+        printf("Невозможно открыть файл устройства: %s\n", DEVICE_FILE_NAME);
+        exit(-1);
+    }
 
-  file_desc = open(DEVICE_FILE_NAME, 0);
-  if (file_desc < 0) {
-    printf("Невозможно открыть файл устройства: %s\n", DEVICE_FILE_NAME);
-    exit(-1);
-  }
-
-  ioctl_get_nth_byte(file_desc);
-  ioctl_get_msg(file_desc);
-  ioctl_set_msg(file_desc, msg);
-
-  close(file_desc);
+    ioctl_get_nth_byte(file_desc);
+    ioctl_get_msg(file_desc);
+    ioctl_set_msg(file_desc, msg);
+    
+    close(file_desc);
 }
