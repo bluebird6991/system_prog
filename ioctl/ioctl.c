5b8958a6b52424a1056ae1a048351bb03051d9b4
AndreyKorchagin <korchagin.andrey.i@ya.ru>
2020-03-13 23:42:21 +0300

    ioctl

--- ioctl/ioctl.c
+++ ioctl/ioctl.c
@@ -9,6 +9,8 @@
  * Определения старшего номера устройства и коды операций ioctl
  */
 #include "chardev.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include <fcntl.h>      /* open */
 #include <unistd.h>     /* exit */
 #include <sys/ioctl.h>  /* ioctl */
@@ -25,7 +27,7 @@ ioctl_set_msg(int file_desc, char *message){
     if (ret_val < 0) {
         printf("Ошибка при вызове ioctl_set_msg: %d\n", ret_val);
         exit(-1);
-      }
+    }
 }
 
 ioctl_get_msg(int file_desc){
@@ -56,14 +58,13 @@ ioctl_get_nth_byte(int file_desc){
     printf("N-ный байт в сообщении (get_nth_byte): ");
 
     i = 0;
-    while (c != 0) {
+    while (c != 0){
         c = ioctl(file_desc, IOCTL_GET_NTH_BYTE, i++);
 
-        if (c < 0) {
+        if (c < 0){
             printf("Ошибка при вызове ioctl_get_nth_byte на %d-м байте.\n", i);
             exit(-1);
         }
-
         putchar(c);
     }
     putchar('\n');
@@ -84,6 +85,6 @@ main(){
     ioctl_get_nth_byte(file_desc);
     ioctl_get_msg(file_desc);
     ioctl_set_msg(file_desc, msg);
-    
+
     close(file_desc);
 }
