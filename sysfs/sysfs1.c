84ac5961173901dce35aa8557ec7998c8e48442d
AndreyKorchagin <korchagin.andrey.i@ya.ru>
2020-03-14 12:57:18 +0300

    sysfs

--- sysfs/sysfs1.c
+++ sysfs/sysfs1.c
@@ -20,7 +20,7 @@ static ssize_t foo_show(struct kobject *kobj,
 	strncpy(buff, foo_tmp, foo_size);
 	//strncat(buff, "\n", 1);
 	//foo_size++;
-	printf("\n");
+	//printf("\n");
 	printk(KERN_ALERT "foo_show buff = %s foo_size = %d foo_tmo = %s\n", buff, foo_size, foo_tmp);
 	return foo_size;
 }
